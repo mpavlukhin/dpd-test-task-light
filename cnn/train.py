77ced1fbf27b83b1b1b53500ca82c65c3c91e189
Max Pavlukhin <pavlukhinm@gmail.com>
2019-11-18 05:31:56 +0300

    Change train.py script for CNN training
    Add read_training_set method for reading
    training set, building_cnn_model for building
    CNN model and its saving, test method
    for prediction checking to train.py script

--- cnn/train.py
+++ cnn/train.py
@@ -4,6 +4,14 @@ import cv2
 import random
 import pickle
 
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+
+import time
+import numpy as np
+
 
 DATADIR = 'raw_data/pet_imgs'
 CATEGORIES = ['dog', 'cat']
@@ -45,10 +53,74 @@ def save_training_set(training_data):
     pickle_out.close()
 
 
+def read_training_set():
+    pickle_in = open('training_set/X.pickle', 'rb')
+    X = pickle.load(pickle_in)
+
+    pickle_in = open('training_set/y.pickle', 'rb')
+    y = pickle.load(pickle_in)
+
+    return X, y
+
+
+def build_cnn_model():
+    name = 'Cats-vs-Dogs-CNN-{}'.format(int(time.time()))
+
+    X, y = read_training_set()
+
+    X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+    X = X / 255.0
+
+    model = Sequential()
+
+    model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    tensorboard = TensorBoard(log_dir='./output/{}'.format(name))
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3, callbacks=[tensorboard])
+
+    model.save('model/cat_vs_dogs.h5')
+
+
+def test():
+    new_model = tf.keras.models.load_model('model/cat_vs_dogs.h5')
+
+    img_array = cv2.imread('raw_data/pet_imgs/cat/100.jpg', cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+
+    assert new_model.predict(np.array(new_array).reshape(-1, IMG_SIZE, IMG_SIZE, 1)) == 1.0
+
+    img_array = cv2.imread('raw_data/pet_imgs/dog/100.jpg', cv2.IMREAD_GRAYSCALE)
+    new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+
+    assert new_model.predict(np.array(new_array).reshape(-1, IMG_SIZE, IMG_SIZE, 1)) == 0.0
+
+    print('Training has completed :-)')
+
+
 def main():
     training_data = create_training_data()
     random.shuffle(training_data)
     save_training_set(training_data)
+
+    build_cnn_model()
+
+    test()
 
 
 if __name__ == "__main__":
